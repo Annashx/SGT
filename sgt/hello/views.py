e7f784e0d25d5b898c48ca5ccd493cf772f12224
Annashx <ana.clara10@aluno.ifce.edu.br>
2023-02-28 17:00:03 -0300

    version nova

--- sgt/hello/views.py
+++ sgt/hello/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request,'index.html')
+ 
 
+def saudacao(request, nome):
+    return HttpResponse("<h1 style='color:blue'> Olá, u ari ispecious </h1> <h1 style='color:red' >%s</h1>  "%(nome))
